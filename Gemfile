757621a55d3f9d31f6bd63bffd6a0093f253c5e0
Corey Norris <coreypnorris@gmail.com>
2014-04-20 08:55:21 -0700

    Initial commit, add basic gems

---
+++ Gemfile
@@ -0,0 +1,32 @@
+source 'https://rubygems.org'
+gem 'rails', '4.0.4'
+gem 'pg'
+gem 'sass-rails', '~> 4.0.2'
+gem 'uglifier', '>= 1.3.0'
+gem 'coffee-rails', '~> 4.0.0'
+gem 'jquery-rails'
+gem 'turbolinks'
+gem 'bootstrap-sass'
+gem 'bcrypt'
+gem 'devise'
+gem 'git'
+
+group :development do
+  gem 'better_errors'
+  gem 'binding_of_caller'
+  gem 'quiet_assets'
+end
+
+group :test, :development do
+  gem 'rspec-rails'
+  gem 'factory_girl_rails'
+  gem 'capybara'
+  gem 'database_cleaner'
+  gem 'launchy'
+  gem 'letter_opener'
+  gem 'faker'
+end
+
+group :test do
+  gem 'shoulda-matchers'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Textpls::Application.config.secret_key_base = '80eeded7d8602fccf51d83f41cc24004adbb4e63390dbea71efb2d362f83fed81f23e69549e537eaa2b74778452ad9cb8bb756879d5622d0d5997a4dd83eb2aa'
