5d54dc9b9d75a9476e0fd00685808968c8df2124
Corey Norris <coreypnorris@gmail.com>
2014-04-20 11:07:14 -0700

    write message spec "dosent save the message if twilio gives an error"

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -4,4 +4,9 @@ describe Message do
   it { should validate_presence_of :to }
   it { should validate_presence_of :from }
   it { should validate_presence_of :body }
+
+  it "dosen't save the message if twilio gives an error" do
+    message = Message.new(:body => 'howdy', :to => '111111', :from => '7754730713')
+    message.save.should be_false
+  end
 end
