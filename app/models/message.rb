cb157246daa7350349db97f909b78ef0749b5c2e
Corey Norris <coreypnorris@gmail.com>
2014-04-20 10:46:35 -0700

    add send_message method to message model

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,4 +2,21 @@ class Message < ActiveRecord::Base
   validates :to, presence: true
   validates :from, presence: true
   validates :body, presence: true
+
+  before_create :send_sms
+
+  private
+
+  def send_message
+    response = RestClient::Request.new(
+      :method => :post,
+      :url => 'https://@api.twilio.com/2010-04-01/Accounts/ACfbd6e240902d8750b0d492eb558ecafc/Messages.json',
+      :user => ENV['TWILIO_ACCOUNT_SID'],
+      :password => ENV['TWILIO_AUTH_TOKEN'],
+      :payload => { :To => to,
+                    :From => from,
+                    :Body => body }
+    ).execute
+  end
+
 end
