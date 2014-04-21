4b0433d5ba880b8ebdaaa91d3fb91272dd51d807
Corey Norris <coreypnorris@gmail.com>
2014-04-20 20:01:02 -0700

    refactor message_spec, now passing

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -13,6 +13,6 @@ describe Message, :vcr => true do
   it "adds an error if the number is invalid" do
     message = Message.new(:body => 'howdy', :to => '111111', :from => '7754730713')
     message.save
-    message.errors.should eq "something bad happened"
+    message.errors[:base].should eq ["The 'To' number 111111 is not a valid phone number."]
   end
 end
